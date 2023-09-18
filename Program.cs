15981f31faab2d210940da26b6baccaf2dd42860
Dilei27 <dilei.alves@hotmail.com>
2023-09-18 10:26:33 -0300

    update

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+
+    {
+        string[] names = { "Dilei", "Osvaldo", "Lupercio" };
+
+        foreach (string name in names)
+        {
+           Console.WriteLine(name);
+        }
+
+    
+    }
+}
+
+
+
+
+
+
+
+
